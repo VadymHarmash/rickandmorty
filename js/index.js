3ace06847781009868f0d7a33545dc8a3f95ce61
Vadym_Harmash_ <102965886+VadymHarmash@users.noreply.github.com>
2023-03-13 20:00:56 +0200

    Added new cards

--- js/index.js
+++ js/index.js
@@ -1,52 +1,41 @@
-const characterData = new Data('https://rickandmortyapi.com/api/character')
-characterData.getData()
-const episodeData = new Data('https://rickandmortyapi.com/api/episode')
-episodeData.getData()
-const locationData = new Data('https://rickandmortyapi.com/api/location')
-locationData.getData()
-
-class Character {
-    constructor(img, name, race) {
-        this.img = img
-        this.name = name
-        this.race = race
+class CardData {
+    constructor(url) {
+        this.url = url
+        this.options = {
+            method: 'GET'
+        }
     }
 
-    createCharacter() {
-        const card = document.createElement('div')
-        card.classList.add('character')
-        const characterImg = document.createElement('img')
-        characterImg.src = this.img
+    getData() {
+        let req = fetch(this.url, this.options)
 
-        const characterName = document.createElement('div')
-        characterName.classList.add('character__name')
-        characterName.append(this.name)
-
-        const characterRace = document.createElement('div')
-        characterRace.classList.add('character__race')
-        characterRace.append(this.race)
-
-        const row = document.querySelector('.main__characters__row')
-        card.append(characterImg, characterName, characterRace)
-        row.append(card)
+        req.then(response => response.json())
+            .then(responseData => {
+                const cardInfo = (createMarkUp(responseData.results))
+                createCard(cardInfo)
+                // searchByAlphabet(cardInfo)
+            })
+            .catch(error => console.error(error))
     }
 }
 
-const rick = new Character('./img/rick.png', 'Rick Sanchez', 'Human')
-rick.createCharacter()
-const rick1 = new Character('./img/rick.png', 'Rick Sanchez2', 'Human')
-rick1.createCharacter()
-const rick2 = new Character('./img/rick.png', 'Rick Sanchez3', 'Human')
-rick2.createCharacter()
-const rick3 = new Character('./img/rick.png', 'Rick Sanchez4', 'Human')
-rick3.createCharacter()
-const rick4 = new Character('./img/rick.png', 'Rick Sanchez5', 'Human')
-rick4.createCharacter()
-const rick5 = new Character('./img/rick.png', 'Rick Sanchez6', 'Human')
-rick5.createCharacter()
-const rick6 = new Character('./img/rick.png', 'Rick Sanchez7', 'Human')
-rick6.createCharacter()
-const rick7 = new Character('./img/rick.png', 'Rick Sanchez8', 'Human')
-rick7.createCharacter()
-const rick8 = new Character('./img/rick.png', 'Rick Sanchez9', 'Human')
-rick8.createCharacter()
+function createMarkUp(data) {
+    return data.filter(value => value)
+}
+
+
+function createCard(cards) {
+    const character = cards.map((card) => {
+        return `<a href="#" class="character">
+                    <img class="character__image" src="${card.image}" alt="API image" width="200" height="200">
+                    <div class="character__name">${card.name}</div>
+                    <div class="character__species">${card.species}</div>
+                </a>`
+        }
+    )
+    const row = document.querySelector('.main__characters__row')
+    row.innerHTML = character.join(' ')
+}
+
+const characterData = new CardData('https://rickandmortyapi.com/api/character')
+characterData.getData()
